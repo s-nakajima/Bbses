e6ef71f523acb2e0c305e875a620822dc5907337
s-nakajima <nakajimashouhei@gmail.com>
2015-05-22 13:26:11 +0900

    Fix to Comment->deleteByBlock() from Comment->deleteAll()

--- Model/Bbs.php
+++ Model/Bbs.php
@@ -283,9 +283,7 @@ class Bbs extends BbsesAppModel {
 			}
 
 			//コメントの削除
-			if (! $this->Comment->deleteAll(array($this->Comment->alias . '.block_key' => $data['Block']['key']), false)) {
-				throw new InternalErrorException(__d('net_commons', 'Internal Server Error'));
-			}
+			$this->Comment->deleteByBlock($data['Block']['key']);
 
 			//Blockデータ削除
 			$this->Block->deleteBlock($data['Block']['key']);
