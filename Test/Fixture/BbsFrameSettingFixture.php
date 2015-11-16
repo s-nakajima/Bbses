b4c2c1e041599ce9bceeb2b3ebf27f1ff597d091
mikikosatou <mikiko.satou@allcreator.net>
2015-11-16 02:07:13 +0000

    テストコード追加

--- Test/Fixture/BbsFrameSettingFixture.php
+++ Test/Fixture/BbsFrameSettingFixture.php
@@ -25,8 +25,8 @@ class BbsFrameSettingFixture extends CakeTestFixture {
 	public $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary', 'comment' => 'ID | | | '),
 		'frame_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'frame key | フレームKey | frames.key | ', 'charset' => 'utf8'),
-		'visible_post_row' => array('type' => 'integer', 'null' => false, 'default' => '10', 'comment' => 'visible row, 1 post or 5, 10, 20, 50, 100 posts | 表示記事数 1件、5件、10件、20件、50件、100件 | | '),
-		'visible_comment_row' => array('type' => 'integer', 'null' => false, 'default' => '10', 'comment' => 'visible row, 1 post or 5, 10, 20, 50, 100 posts | 表示記事数 1件、5件、10件、20件、50件、100件 | | '),
+		'articles_per_page' => array('type' => 'integer', 'null' => false, 'default' => '10', 'unsigned' => false, 'comment' => 'artcles visible row, 1, 5, 10, 20, 50, 100 | 表示記事数 1件、5件、10件、20件、50件、100件 | | '),
+		'comments_per_page' => array('type' => 'integer', 'null' => false, 'default' => '10', 'unsigned' => false, 'comment' => 'comments visible row, 1 post or 5, 10, 20, 50, 100 | 表示記事数 1件、5件、10件、20件、50件、100件 | | '),
 		'created_user' => array('type' => 'integer', 'null' => true, 'default' => '0', 'comment' => 'created user | 作成者 | users.id | '),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'created datetime | 作成日時 | | '),
 		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => '0', 'comment' => 'modified user | 更新者 | users.id | '),
@@ -46,8 +46,8 @@ class BbsFrameSettingFixture extends CakeTestFixture {
 		array(
 			'id' => 1,
 			'frame_key' => 'frame_1',
-			'visible_post_row' => 10,
-			'visible_comment_row' => 10,
+			'articles_per_page' => 10,
+			'comments_per_page' => 10,
 			'created_user' => 1,
 			'created' => '2014-06-18 02:06:22',
 			'modified_user' => 1,
