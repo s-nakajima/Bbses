35b900054fa91eafb4dd028e78b5a6360024533b
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-07-22 18:59:49 +0900

    テストデータ見直し - BlockSettingForBbsFixture

--- Test/Fixture/BlockSettingForBbsFixture.php
+++ Test/Fixture/BlockSettingForBbsFixture.php
@@ -37,4 +37,121 @@ class BlockSettingForBbsFixture extends BlockSettingFixture {
  */
 	public $table = 'block_settings';
 
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		// プラグインデフォルト値 - room_idなし、block_keyなし
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => null,
+			'block_key' => null,
+			'field_name' => BlockSettingBehavior::FIELD_USE_COMMENT,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => null,
+			'block_key' => null,
+			'field_name' => BlockSettingBehavior::FIELD_USE_LIKE,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => null,
+			'block_key' => null,
+			'field_name' => BlockSettingBehavior::FIELD_USE_UNLIKE,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+		// use_workflow, use_comment_approvalの初期値は、rooms.need_approvalによって値決まる。
+		// BlockSettingでデフォルト値（room_id=null, block_key=nullの値）設定しても無視される。
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => null,
+			'block_key' => null,
+			'field_name' => BlockSettingBehavior::FIELD_USE_WORKFLOW,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_NUMERIC,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => null,
+			'block_key' => null,
+			'field_name' => BlockSettingBehavior::FIELD_USE_COMMENT_APPROVAL,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_NUMERIC,
+		),
+		// ブロック設定後 - room_idあり、block_keyあり
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => 1,
+			'block_key' => 'block_1',
+			'field_name' => BlockSettingBehavior::FIELD_USE_COMMENT,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => 1,
+			'block_key' => 'block_1',
+			'field_name' => BlockSettingBehavior::FIELD_USE_LIKE,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => 1,
+			'block_key' => 'block_1',
+			'field_name' => BlockSettingBehavior::FIELD_USE_UNLIKE,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => 1,
+			'block_key' => 'block_1',
+			'field_name' => BlockSettingBehavior::FIELD_USE_WORKFLOW,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_NUMERIC,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => 1,
+			'block_key' => 'block_1',
+			'field_name' => BlockSettingBehavior::FIELD_USE_COMMENT_APPROVAL,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_NUMERIC,
+		),
+		// イレギュラーデータ - room_idあり、block_keyあり、USE_WORKFLOW, USE_COMMENT_APPROVALのデータなし
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => 2,
+			'block_key' => 'block_2',
+			'field_name' => BlockSettingBehavior::FIELD_USE_COMMENT,
+			'value' => '0',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => 2,
+			'block_key' => 'block_2',
+			'field_name' => BlockSettingBehavior::FIELD_USE_LIKE,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+		array(
+			'plugin_key' => 'bbses',
+			'room_id' => 2,
+			'block_key' => 'block_2',
+			'field_name' => BlockSettingBehavior::FIELD_USE_UNLIKE,
+			'value' => '1',
+			'type' => BlockSettingBehavior::TYPE_BOOLEAN,
+		),
+	);
+
 }
