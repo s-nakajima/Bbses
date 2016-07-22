9490cd5a7b09967ad007410b776df6b3bf253510
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-07-22 19:42:16 +0900

    マイグレーション - delete_bbs_settings フィクスチャー削除 - BbsSettingFixture

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -120,30 +120,6 @@ class BbsesSchema extends CakeSchema {
 	);
 
 /**
- * bbs_settings table
- *
- * @var string
- */
-	public $bbs_settings = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID'),
-		'bbs_key' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index', 'collate' => 'utf8_general_ci', 'comment' => '掲示板キー', 'charset' => 'utf8'),
-		'use_workflow' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'comment' => '記事の承認機能 0:使わない 1:使う'),
-		'use_comment' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'comment' => 'コメント機能 0:使わない 1:使う'),
-		'use_comment_approval' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'comment' => 'コメントの承認機能 0:使わない 1:使う'),
-		'use_like' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'comment' => 'いいねボタンの使用 0:使わない 1:使う'),
-		'use_unlike' => array('type' => 'boolean', 'null' => false, 'default' => '1', 'comment' => 'わるいねボタンの使用 0:使わない 1:使う'),
-		'created_user' => array('type' => 'integer', 'null' => true, 'default' => '0', 'unsigned' => false, 'comment' => '作成者'),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '作成日時'),
-		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => '0', 'unsigned' => false, 'comment' => '更新者'),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '更新日時'),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'bbs_key' => array('column' => 'bbs_key', 'unique' => 0)
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
-
-/**
  * bbses table
  *
  * @var string
